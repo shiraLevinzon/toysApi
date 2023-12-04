cb2aeb8aebe9836dced41b05147414866409ac09
shiraLevinzon <levinzon@g.jct.ac.il>
2023-12-04 13:01:25 +0200

    app fixes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const cors=require("cors");
 const app= express();
 const toyRoutes= require("./routes/toy.routes")
 const userRoutes= require("./routes/user.routes")
-
+const path= require("path");
 app.use(cors());
 app.use(express.json());
 
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/toys",toyRoutes);
 app.use("/api/v1/users",userRoutes);
 
